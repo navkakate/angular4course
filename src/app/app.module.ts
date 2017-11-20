3b2eac0bc26da13e92a4cec949a64698f0cfb602
Kateryna Lopushanska <kateryna.lopushanska@thomascookonline.com>
2017-11-20 15:20:25 +0100

    first commit with dummy componenents

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './header/header.component';
+import { RecipeList } from './recipe.book/recipe.list/recipe.list.component';
+import { RecipeItem } from './recipe.book/recipe.item/recipe.item.component';
+import { RecipeDetail } from './recipe.book/recipe.detail/recipe.detail.component';
+import { ShoppingList } from './shopping.list/shopping.list/shopping.list.component';
+import { ShoppingListEdit } from './shopping.list/shopping.list.edit/shopping.list.edit.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipeList,
+    RecipeItem,
+    RecipeDetail,
+    ShoppingList,
+    ShoppingListEdit
   ],
   imports: [
     BrowserModule
